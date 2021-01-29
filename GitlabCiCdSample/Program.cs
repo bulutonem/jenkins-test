1384a3bc750ef62af96035e44341a1dce3c7ccd8
bulut <onem_bulut@hotmail.com>
2021-01-29 23:26:57 +0300

    update

--- GitlabCiCdSample/Program.cs
+++ GitlabCiCdSample/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Timers;
 
 namespace GitlabCiCdSample
 {
     class Program
     {
+        private static Timer timer = new Timer();
         static void Main(string[] args)
         {
+            timer.Elapsed += Timer_Elapsed;
             Console.WriteLine("Hello World!");
+            timer.Interval = TimeSpan.FromSeconds(30).TotalMilliseconds;
+            timer.Start();
+            Console.ReadLine();
+        }
+
+        private static void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine(DateTime.Now.ToString());
         }
     }
 }
